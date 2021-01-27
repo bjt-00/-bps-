b6444077f121d0378b5af9103896f6bef45e28e3
Abdul Kareem <abdulkareem.bitguiders@gmail.com>
2021-01-26 19:42:40 -0500

    BPOS-26: Http to Https conversion

--- view/contents/email/templates/accountActivationTemplate.php
+++ view/contents/email/templates/accountActivationTemplate.php
@@ -13,12 +13,12 @@
 <table style="width:90%;border-left:3px solid #e1e1e1;border-right:3px solid #e1e1e1;border-top:3px solid #e1e1e1;" align="center" cellpadding="0" cellspacing="0">
 <tr>
 	<td colspan="2" style="border-bottom:3px solid orange;text-align:center">
-		   <a href="http://www.bitguiders.com/bpos"  target="_new">
-			<img src="http://www.bitguiders.com/bpos/themes/common/images/bposLogo.png" alt="BPOS"/>
+		   <a href="https://www.bitguiders.com/bpos"  target="_new">
+			<img src="https://www.bitguiders.com/bpos/themes/common/images/bposLogo.png" alt="BPOS"/>
 		   </a>
 	</td>
 </tr>
-<tr style="background-image: url('http://www.bitguiders.com/themes/default/images/contents.png'); ">
+<tr style="background-image: url('https://www.bitguiders.com/themes/default/images/contents.png'); ">
 	<td style="width:67%;padding:15px;margin-top: 0px;text-align:center" valign="top">
 		   <h1>Thanks for joining <span style="color:rgb(231,118,28)">b</span><span style="color:rgb(39,51,65)">pos</span>!</h1>
 		   <p style="font-size:15px;font-family:verdana">
@@ -32,7 +32,7 @@
 			  <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.
 			 </p>
 			</p>
-			 <a href="http://www.bitguiders.com/bpos/rest/services/company.php?cac=<COMPANY_ACTIVATION_CODE>" target="_new" style="background-color:rgb(231,118,28);color:white;border:8px solid rgb(231,118,28);height:150px;width:230px;font-family:verdana;text-decoration:none">
+			 <a href="https://www.bitguiders.com/bpos/rest/services/company.php?cac=<COMPANY_ACTIVATION_CODE>" target="_new" style="background-color:rgb(231,118,28);color:white;border:8px solid rgb(231,118,28);height:150px;width:230px;font-family:verdana;text-decoration:none">
 			 Activate Account
 			 </a>
 			</p>
@@ -50,21 +50,21 @@
 			 If you encounter any issues, please reach our help center at 
 			 <a href="mailto:info@bitguiders.com">info@bitguiders.com</a>
 			 </p>
-			<a href="http://www.bitguiders.com"  target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/bitguiders.png" alt="bitguiders" style="width:100px;height:35px;border:0px" />
+			<a href="https://www.bitguiders.com"  target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/bitguiders.png" alt="bitguiders" style="width:100px;height:35px;border:0px" />
 			</a>
 			<br>
 			<b>Phone:</b>+1 (412)-482-2861<br>
 			<b>Email:</b> <a href="mailto:info@bitguiders.com">info@bitguiders.com</a> <br>
 			
-			<a href="http://www.facebook.com/pages/bitguiders/181232348602166"  target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/facebook.gif" style="border:0px" />
+			<a href="https://www.facebook.com/pages/bitguiders/181232348602166"  target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/facebook.gif" style="border:0px" />
 			</a>
-			<a href="http://pk.linkedin.com/in/bitguiders" style="text-decoration:none" target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/linkedin.gif" style="border:0px"/>
+			<a href="https://pk.linkedin.com/in/bitguiders" style="text-decoration:none" target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/linkedin.gif" style="border:0px"/>
 			</a>
-			<a href="http://twitter.com/bitguiders" target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/twitter.gif" style="border:0px"/>
+			<a href="https://twitter.com/bitguiders" target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/twitter.gif" style="border:0px"/>
 			</a>
 	</td>
 </tr>
--- view/contents/email/templates/forgotPasswordTemplate.php
+++ view/contents/email/templates/forgotPasswordTemplate.php
@@ -13,12 +13,12 @@
 <table style="width:90%;border-left:3px solid #e1e1e1;border-right:3px solid #e1e1e1;border-top:3px solid #e1e1e1;" align="center" cellpadding="0" cellspacing="0">
 <tr>
 	<td colspan="2" style="border-bottom:3px solid orange;text-align:center">
-		   <a href="http://www.bitguiders.com/bpos"  target="_new">
-			<img src="http://www.bitguiders.com/bpos/themes/common/images/bposLogo.png" alt="BPOS"/>
+		   <a href="https://www.bitguiders.com/bpos"  target="_new">
+			<img src="https://www.bitguiders.com/bpos/themes/common/images/bposLogo.png" alt="BPOS"/>
 		   </a>
 	</td>
 </tr>
-<tr style="background-image: url('http://www.bitguiders.com/themes/default/images/contents.png'); ">
+<tr style="background-image: url('https://www.bitguiders.com/themes/default/images/contents.png'); ">
 	<td style="width:67%;padding:15px;margin-top: 0px;text-align:center" valign="top">
 		   <h1>Forgot Password!</h1>
 		   <p style="font-size:15px;font-family:verdana">
@@ -33,21 +33,21 @@
 			 If you have not requested, please reach our help center at 
 			 <a href="mailto:info@bitguiders.com">info@bitguiders.com</a>
 			 </p>
-			<a href="http://www.bitguiders.com"  target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/bitguiders.png" alt="bitguiders" style="width:100px;height:35px;border:0px" />
+			<a href="https://www.bitguiders.com"  target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/bitguiders.png" alt="bitguiders" style="width:100px;height:35px;border:0px" />
 			</a>
 			<br>
 			<b>Phone:</b>+1 (412)-482-2861<br>
 			<b>Email:</b> <a href="mailto:info@bitguiders.com">info@bitguiders.com</a> <br>
 			
-			<a href="http://www.facebook.com/pages/bitguiders/181232348602166"  target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/facebook.gif" style="border:0px" />
+			<a href="https://www.facebook.com/pages/bitguiders/181232348602166"  target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/facebook.gif" style="border:0px" />
 			</a>
-			<a href="http://pk.linkedin.com/in/bitguiders" style="text-decoration:none" target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/linkedin.gif" style="border:0px"/>
+			<a href="https://pk.linkedin.com/in/bitguiders" style="text-decoration:none" target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/linkedin.gif" style="border:0px"/>
 			</a>
-			<a href="http://twitter.com/bitguiders" target="_new">
-			<img src="http://www.bitguiders.com/themes/default/images/twitter.gif" style="border:0px"/>
+			<a href="https://twitter.com/bitguiders" target="_new">
+			<img src="https://www.bitguiders.com/themes/default/images/twitter.gif" style="border:0px"/>
 			</a>
 	</td>
 </tr>
